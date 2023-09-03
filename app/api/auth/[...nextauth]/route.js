29c0db7347aa47ca0dd19c112c7ba078133fcf15
Airshock72 <gelagela2222@gmail.com>
2023-09-03 19:53:02 +0400

    set up database functions and models

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
+import { connectToDB } from '@/utils/database'
 import GoogleProvider from 'next-auth/providers/google'
 import NextAuth from 'next-auth'
+import User from '@/models/user'
 
 const handler = NextAuth({
   providers: [
@@ -8,7 +10,33 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
-  async session({ session }) {},
-  async SignIn({ profile }){},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email
+    })
+    session.user.id = sessionUser._id.toString()
+    return session
+  },
+  async SignIn({ profile }){
+    try {
+      await connectToDB()
+      //check is user already exists
+      const userExists = await User.findOne({
+        email: profile.email
+      })
+      //if not, create a new user
+      if(!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(' ', '').toLowerCase(),
+          image: profile.picture
+        })
+      }
+      return true
+    } catch (err) {
+      console.log(err)
+      return false
+    }
+  },
 })
 export { handler as GET, handler as POST }
